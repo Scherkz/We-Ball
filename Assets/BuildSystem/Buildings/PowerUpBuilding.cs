aa62de8f09185f9739fedbed9cd442085451503c
JostMK <jost.klink@gmail.com>
2025-12-17 14:34:32 +0100

    Remove old unused logic

--- Assets/BuildSystem/Buildings/PowerUpBuilding.cs
+++ Assets/BuildSystem/Buildings/PowerUpBuilding.cs
@@ -3,43 +3,20 @@ using UnityEngine;
 public abstract class PowerUpBuilding : Building
 {
     [SerializeField] protected GameObject collectVfxPrefab;
-    
-    private bool isCollected;
-    
-    private SpriteRenderer[] renderers = { };
-    private Collider2D[] colliders = { };
-    
-    //To activate in GameManager, to reset all the hidden power-ups
-    public virtual void ResetForNextRound()
-    {
-        isCollected = false;
-        SetVisibility(true);
-    }
-    
-    protected virtual void Awake()
-    {
-        renderers = GetComponentsInChildren<SpriteRenderer>(true);
-        colliders = GetComponentsInChildren<Collider2D>(true);
 
-        foreach (var col in colliders)
-        {
-            col.isTrigger = true;
-        }
-    }
-    
+    private bool isCollected;
+
     protected virtual void OnTriggerEnter2D(Collider2D collider)
     {
         if (isCollected)
             return;
-        
+
         if (!collider.gameObject.CompareTag("Player"))
-                    return;
-        var pickupPlayer = collider.GetComponentInParent<Player>();
-        
-        var controller = collider.GetComponent<PlayerController>();
-        if (controller == null)
             return;
-        
+
+        var pickupPlayer = collider.GetComponentInParent<Player>();
+        var controller = pickupPlayer.GetPlayerController();
+
         isCollected = true;
 
         if (collectVfxPrefab != null)
@@ -48,31 +25,17 @@ public abstract class PowerUpBuilding : Building
         }
 
         OnCollected(pickupPlayer, controller);
-        
+
         var buildGrid = GetComponentInParent<BuildGrid>();
         if (buildGrid != null)
         {
             buildGrid.RemoveBuildingAtPosition(transform.position);
             return;
         }
-        
+
         Destroy(gameObject);
     }
-    
+
     //should be overwritten by each power up
     protected abstract void OnCollected(Player player, PlayerController controller);
-    
-    //activates or deactivates all the collider and sprites 
-    private void SetVisibility(bool value)
-    {
-        foreach (var renderer in renderers)
-        {
-            renderer.enabled = value;
-        }
-        
-        foreach (var col in colliders)
-        {
-            col.enabled = value;
-        }
-    }
 }
