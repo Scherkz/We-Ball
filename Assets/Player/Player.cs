7e4dfcdd9f2e37528fdd9edc4451ebd50824e280
Owndbyfreak <ma.wohlfarth@gmx.de>
2026-01-31 11:47:30 +0100

    Refactor player

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -270,12 +270,6 @@ public class Player : MonoBehaviour
 
     private void OnPlayerSwings()
     {
-        if(playerInput.currentActionMap == null || playerInput.currentActionMap.name != playingActionMapName)
-            return;
-        
-        if(hasFinishedRound)
-            return;
-        
         numberOfSwingsThisRound++;
 
         if (numberOfSwingsThisRound >= surrenderHintSwingsThreshold)
