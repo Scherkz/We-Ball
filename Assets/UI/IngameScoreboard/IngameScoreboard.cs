3288dd96e4ab6ec862a6f9f390a66a01aa37c664
Scherkz <96747456+Scherkz@users.noreply.github.com>
2026-02-02 12:12:45 +0100

    Fix IngameScoreboard invisible after new round (#129)

--- Assets/UI/IngameScoreboard/IngameScoreboard.cs
+++ Assets/UI/IngameScoreboard/IngameScoreboard.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class IngameScoreboard : MonoBehaviour
 {
     [SerializeField] private GameObject playerScoreCounterPrefab;
-    
+
     [SerializeField] private PlayerRegistry playerRegistry;
 
     public void ResetSelf()
@@ -53,8 +53,10 @@ public class IngameScoreboard : MonoBehaviour
         }
     }
 
-    private void OnLevelLoaded(Level _level, bool _isLobby)
+    private void OnLevelLoaded(Level _level, bool isLobby)
     {
         ResetSelf();
+        if (!isLobby)
+            SetupScoreboard();
     }
 }
